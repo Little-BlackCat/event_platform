30e93b07509ddaba207d0b280f2c9c064d2fa15e
BlackCat <neko.sword@gmail.com>
2024-01-22 18:12:21 +0700

    add: card component

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -32,7 +32,11 @@ export default function Collection({
 
               return (
                 <li key={event._id} className="flex justify-center">
-                  <Card />
+                  <Card 
+                    event={event} 
+                    hasOrderLink={hasOrderLink}
+                    hidePrice={hidePrice}
+                  />
                 </li>
               );
             })}
